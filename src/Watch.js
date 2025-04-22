4ee161329f47fdb9014fc251a3a49348a0877ce4
coderceder61 <codercoder61@gmail.com>
2025-04-22 20:07:36 +0100

    Update Watch.js

--- src/Watch.js
+++ src/Watch.js
@@ -7,7 +7,7 @@ import Nav from './Nav'
 import Cookies from 'js-cookie';
 import Hls from 'hls.js';
 import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
+//import 'video.js/dist/video-js.css';
 const pageSize = 10; // Number of items per page
 
 const Watch = forwardRef((props, ref) => {

c6812a2c25383fc5a44b7a8748c897ef0c6305c2
coderceder61 <codercoder61@gmail.com>
2025-04-22 19:11:14 +0100

    Update Watch.js

--- src/Watch.js
+++ src/Watch.js
@@ -139,11 +139,8 @@ const response2 = await axios.post('https://proxy-production-ddb5.up.railway.app
         console.log(response2);
 
         const videoUrl = "https://hianimeproxy-production.up.railway.app/m3u8-proxy?url=" + response2.data.content.data.sources[0].url;
-        //console.log("Video URL: ", videoUrl);
-const encodedURL = encodeURIComponent(response2.data.content.data.tracks[0].file);
-const proxyURL = `/api/subtitle?url=${encodedURL}`;
+        setTracks(response2.data.content.data.tracks)
 
-track.current.src = proxyURL
 
         if (Hls.isSupported()) {
           hls = new Hls();

9e1106fb450c45f02051a7fc46a9d7139f78516a
coderceder61 <codercoder61@gmail.com>
2025-04-22 17:32:10 +0100

    Update Watch.js

--- src/Watch.js
+++ src/Watch.js
@@ -226,7 +226,7 @@ useEffect(() => {
 
          
 
-	 <video key={tracks?.length || 0} src="" id="player" ref={videoRef} controls>
+	 <video src="" id="player" ref={videoRef} controls>
   {
     tracks && tracks.map((track, index) => (
       <track

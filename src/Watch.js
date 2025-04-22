b4b33dcc27968ba4b8457e546bc0e831ec3135f2
coderceder61 <codercoder61@gmail.com>
2025-04-22 16:52:01 +0100

    Update Watch.js

--- src/Watch.js
+++ src/Watch.js
@@ -238,15 +238,14 @@ response2 = await axios.post('https://proxy-production-ddb5.up.railway.app/fetch
 
 	 <video src="" id="player" ref={videoRef} controls>
   {
-    tracks!==null && tracks.map((track, index) => (
+    tracks!==null ? tracks.map((track, index) => (
       <track
         key={index}
         src={`/api/subtitle?url=${encodeURIComponent(track.file)}`}
         kind="subtitles"
-        srcLang="en"
         label={track.label}
       />
-    ))
+    )) : <span>tracks is null</span>
   }
 </video>
 

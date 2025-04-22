3f28e5e87cf89c596a3211cc522b8ed3b0e94661
coderceder61 <codercoder61@gmail.com>
2025-04-22 18:59:22 +0100

    Update Watch.js

--- src/Watch.js
+++ src/Watch.js
@@ -10,7 +10,7 @@ import Hls from 'hls.js';
 const pageSize = 10; // Number of items per page
 
 const Watch = forwardRef((props, ref) => {
-
+const [tracks,setTracks]=useState([])
 const track = useRef(null);
 
 	const fetchEpisodeSources = async (episodeId) => {
@@ -22,8 +22,9 @@ const response2 = await axios.post('https://proxy-production-ddb5.up.railway.app
 
 console.log(response2);
       const videoUrl = "https://hianimeproxy-production.up.railway.app/m3u8-proxy?url=" + response2.data.content.data.sources[0].url;
-		const encodedURL = encodeURIComponent(response2.data.content.data.tracks[0].file);
-const proxyURL = `/api/subtitle?url=${encodedURL}`;
+		
+		await setTracks(response2.data.content.data.tracks)
+
 
 track.current.src = proxyURL
 
@@ -224,14 +225,15 @@ track.current.src = proxyURL
          
 
 	  <video src="" id="player" ref={videoRef} controls>
-	  <track
-        src=""
-ref={track}
-        kind="subtitles"
-        srcLang="en"
-        label="English"
-        default
-      />
+	  {tracks && tracks.map((track,index)=>{
+		  return(
+			<track
+        src=`/api/subtitle?url=${encodeURIComponent(track.file)}`
+	        kind="subtitles"
+	        label={track.label}
+	      />  
+		  )
+	  }) }
 	</video>
          
           <div ref={spinnerRef} className="spinner-container">
